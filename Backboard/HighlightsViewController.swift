02a153a966cb4d5c92bfecf1cd26fe476ade4860
Carson Saldanha <carson.saldanha@gmail.com>
2021-10-18 00:19:34 -0500

    Highlights groundwork

--- Backboard/HighlightsViewController.swift
+++ Backboard/HighlightsViewController.swift
@@ -6,24 +6,79 @@
 //
 
 import UIKit
+import WebKit
+import Alamofire
+import SwiftyJSON
 
-class HighlightsViewController: UIViewController {
+class HighlightsTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var highlightVideoWebView: WKWebView!
+    @IBOutlet weak var highlightTitleLabel: UILabel!
+    
+}
 
+class HighlightsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var highlightsTableView: UITableView!
+    
+    let highlightsCellIdentifier = "Highlights Cell"
+    var highlightTitles: [String] = []
+    var highlightLinks: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        highlightsTableView.delegate = self
+        highlightsTableView.dataSource = self
+        fetchHighlights()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // Returns the number of highlights in the table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //return highlightTitles.count
+        return 1
     }
-    */
+    
+    // Sets the highlight title and video of each cell in the table
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: highlightsCellIdentifier, for: indexPath) as! HighlightsTableViewCell
+        let row = indexPath.row
+//        cell.highlightTitleLabel?.text = highlightTitles[row]
+        cell.highlightTitleLabel?.text = "Testing"
+        
+        let videoHTML = "<div style=\"width: 100%; height: 0px; position: relative; padding-bottom: 56.250%;\">" +
+                        "<iframe src=\"https://streamable.com/e/ndkvfn\" frameborder=\"0\" width=\"100%\" height=\"100%\"" +
+                        "allowfullscreen style=\"width: 100%; height: 100%; position: absolute;\"></iframe></div>"
+        cell.highlightVideoWebView.loadHTMLString(videoHTML, baseURL: nil)
+        return cell
+    }
+    
+    // Pulls NBA articles from the news API and updates the table
+    func fetchHighlights() {
+        // Parse this? https://www.reddit.com/search/?q=subreddit%3Anba%20site%3Astreamable.com&sort=new
+        
+
+//        AF.request(redditAPI, method: .get).validate().responseJSON { response in
+//            switch response.result {
+//            case .success(let value):
+//                let json = JSON(value)
+//                // Only parse the first 25 articles and add the data to the arrays
+//                for article in 0...24 {
+//                    if let title = json["articles"][article]["title"].string {
+//                        self.highlightTitles.append(title)
+//                    }
+//                    if let url = json["articles"][article]["url"].string {
+//                        self.highlightLinks.append(url)
+//                    }
+//                }
+//            case .failure(let error):
+//                print(error)
+//            }
+//            // Reload the table after the API request is compelete
+//            DispatchQueue.main.async {
+//                self.highlightsTableView.reloadData()
+//            }
+//        }
+    }
 
 }
+
--- Backboard/NewsViewController.swift
+++ Backboard/NewsViewController.swift
@@ -20,7 +20,7 @@ class NewsViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     @IBOutlet weak var newsTableView: UITableView!
     
-    let newsCellIdentifier = "NewsCell"
+    let newsCellIdentifier = "News Cell"
     var articleTitles: [String] = []
     var articleImages: [String] = []
     var articleLinks: [String] = []
@@ -32,10 +32,12 @@ class NewsViewController: UIViewController, UITableViewDelegate, UITableViewData
         fetchArticles()
     }
     
+    // Returns the number of articles in the table
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return articleTitles.count
     }
     
+    // Sets the article title, image, and URL of each cell in the table
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: newsCellIdentifier, for: indexPath) as! NewsTableViewCell
         let row = indexPath.row
@@ -46,22 +48,27 @@ class NewsViewController: UIViewController, UITableViewDelegate, UITableViewData
         return cell
     }
     
+    // Opens the link of the article in the user's browser if the cell is selected
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let row = indexPath.row
         guard let articleUrl = URL(string: articleLinks[row]) else { return }
         UIApplication.shared.open(articleUrl)
     }
     
+    // Pulls NBA articles from the news API and updates the table
     func fetchArticles() {
         let newsAPI = "https://newsapi.org/v2/everything?" +
-                  "q=NBA&" +
-                  "from=2021-10-12&" + // change date
-                  "sortBy=popularity&" +
-                  "apiKey=bd2de92e7e834d6481f1c8f279d4add0"
+                      "q=nba&" +
+                      "domains=espn.com,cbssports.com,nbcsports.com,nba.com,bleacherreport.com" +
+                      "from=" + getTodaysDate() + "&" +
+                      "to=" + getTodaysDate() + "&" +
+                      "sortBy=popularity&" +
+                      "apiKey=bd2de92e7e834d6481f1c8f279d4add0"
         AF.request(newsAPI, method: .get).validate().responseJSON { response in
             switch response.result {
             case .success(let value):
                 let json = JSON(value)
+                // Only parse the first 25 articles and add the data to the arrays
                 for article in 0...24 {
                     if let title = json["articles"][article]["title"].string {
                         self.articleTitles.append(title)
@@ -76,10 +83,19 @@ class NewsViewController: UIViewController, UITableViewDelegate, UITableViewData
             case .failure(let error):
                 print(error)
             }
+            // Reload the table after the API request is compelete
             DispatchQueue.main.async {
                 self.newsTableView.reloadData()
             }
         }
+    }
+    
+    // Helper function to get today's date for the news API
+    private func getTodaysDate() -> String {
+        let date = Date()
+        let format = DateFormatter()
+        format.dateFormat = "yyyy-MM-dd"
+        return format.string(from: date)
     }
 
 }
