37ea5c9a0abe923b92ccd7da82093b67a737a0c2
Carson Saldanha <carson.saldanha@gmail.com>
2021-10-21 14:40:25 -0500

    Alpha release now complete (Tweaked login and added keyboard hiding code)

--- Backboard/LogInViewController.swift
+++ Backboard/LogInViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Firebase
 
-class LogInViewController: UIViewController {
+class LogInViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var logInButton: UIButton!
     @IBOutlet weak var backButton: UIButton!
@@ -17,17 +17,22 @@ class LogInViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Used to hide the software keyboard
+        emailField.delegate = self
+        passwordField.delegate = self
+        
+        // Changes the color and shape of the login and signup buttons
         logInButton.backgroundColor = UIColor.init(red: 231/255, green: 51/255, blue: 55/255, alpha: 1)
         logInButton.layer.cornerRadius = 25.0
         logInButton.tintColor = UIColor.white
         backButton.tintColor = UIColor.init(red: 231/255, green: 51/255, blue: 55/255, alpha: 1)
         
-        //Moves to the tab bar controller if we sucessfully sign in
+        // Moves to the tab bar controller if we sucessfully sign in
         Auth.auth().addStateDidChangeListener { auth, user in
-            if user != nil {
-              self.emailField.text = nil
-              self.passwordField.text = nil
+            if (user != nil) {
+                self.emailField.text = nil
+                self.passwordField.text = nil
                 
                 let storyboard = UIStoryboard(name: "Main", bundle: nil)
                 let mainTabBarController = storyboard.instantiateViewController(identifier: "TabBarController")
@@ -38,7 +43,7 @@ class LogInViewController: UIViewController {
     }
     
     @IBAction func pressedLogIn(_ sender: Any) {
-        //checks if the email and password texts are filled
+        // Checks if the email and password texts are filled
         guard let email = emailField.text,
               let password = passwordField.text,
               email.count > 0,
@@ -47,11 +52,11 @@ class LogInViewController: UIViewController {
             return
         }
         
-        //trying to log in
+        // Trying to log in
         Auth.auth().signIn(withEmail: email, password: password) {
             user, error in
             if let error = error, user == nil {
-                //there is an error with our login credentials
+                // There is an error with our login credentials
                 let alert = UIAlertController(title: "Sign in failed", message: error.localizedDescription, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .default))
                 self.present(alert, animated: true, completion: nil)
@@ -59,9 +64,20 @@ class LogInViewController: UIViewController {
         }
     }
     
-    //go back to the signup page if the "Back" button is pressed
+    // Go back to the signup page if the "Back" button is pressed
     @IBAction func pressedBackButton(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
-}
+    
+    // Hides the software keyboard when pressing return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
 
+    // Hides the software keyboard when tapping on the background
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+
+}
--- Backboard/SignUpViewController.swift
+++ Backboard/SignUpViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import Firebase
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var logInButton: UIButton!
@@ -17,26 +17,22 @@ class SignUpViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Used to hide the software keyboard
+        emailField.delegate = self
+        passwordField.delegate = self
 
-        //Changes the color and shape of the login and signup buttons
+        // Changes the color and shape of the login and signup buttons
         signUpButton.backgroundColor = UIColor.init(red: 231/255, green: 51/255, blue: 55/255, alpha: 1)
         signUpButton.layer.cornerRadius = 25.0
         signUpButton.tintColor = UIColor.white
         logInButton.tintColor = UIColor.init(red: 231/255, green: 51/255, blue: 55/255, alpha: 1)
         
-        //Log out every time we start the app
-        do {
-            try Auth.auth().signOut()
-        }
-        catch {
-            print(error)
-        }
-        
-        //Moves to the tab bar controller if we sucessfully sign up
+        // Moves to the tab bar controller if we sucessfully sign up
         Auth.auth().addStateDidChangeListener { auth, user in
-            if user != nil {
-              self.emailField.text = nil
-              self.passwordField.text = nil
+            if (user != nil) {
+                self.emailField.text = nil
+                self.passwordField.text = nil
                 
                 let storyboard = UIStoryboard(name: "Main", bundle: nil)
                 let mainTabBarController = storyboard.instantiateViewController(identifier: "TabBarController")
@@ -47,7 +43,7 @@ class SignUpViewController: UIViewController {
     }
     
     @IBAction func pressedSignUp(_ sender: Any) {
-        //checks if the email and password texts are filled
+        // Checks if the email and password texts are filled
         guard let email = emailField.text,
               let password = passwordField.text,
               email.count > 0,
@@ -56,15 +52,15 @@ class SignUpViewController: UIViewController {
             return
         }
         
-        //try to create a new user
+        // Try to create a new user
         Auth.auth().createUser(withEmail: email, password: password) {
             user, error in
             if error == nil {
-                //Creation sucessful, logging in after account creation
+                // Creation sucessful, logging in after account creation
                 Auth.auth().signIn(withEmail: email, password: password)
             }
             else {
-                //There is an error
+                // There is an error
                 let alert = UIAlertController(title: "Sign in failed", message: error?.localizedDescription, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .default))
                 self.present(alert, animated: true, completion: nil)
@@ -76,4 +72,16 @@ class SignUpViewController: UIViewController {
         emailField.text = nil
         passwordField.text = nil
     }
+    
+    // Hides the software keyboard when pressing return
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+
+    // Hides the software keyboard when tapping on the background
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
 }
