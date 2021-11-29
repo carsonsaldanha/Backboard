d6d485971b7399fb1018e4480d979f9842ef6e4c
Carson Saldanha <carson.saldanha@gmail.com>
2021-11-29 15:58:55 -0600

    Settings screen constraints

--- Backboard/SettingsViewController.swift
+++ Backboard/SettingsViewController.swift
@@ -14,10 +14,11 @@ class SettingsViewController: UIViewController, UNUserNotificationCenterDelegate
     @IBOutlet weak var signOutButton: UIButton!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var favoriteTeamLabel: UILabel!
-    @IBOutlet weak var notificationsSwitch: UISwitch!
+    @IBOutlet weak var themeLabel: UILabel!
     @IBOutlet weak var systemButton: DLRadioButton!
     @IBOutlet weak var lightButton: DLRadioButton!
     @IBOutlet weak var darkButton: DLRadioButton!
+    @IBOutlet weak var notificationsSwitch: UISwitch!
     
     var currentView: UIUserInterfaceStyle!
     var updateView: UIUserInterfaceStyle!
@@ -27,49 +28,48 @@ class SettingsViewController: UIViewController, UNUserNotificationCenterDelegate
         super.viewDidLoad()
         
         UNUserNotificationCenter.current().delegate = self
-
-        // Do any additional setup after loading the view.
         
         // Design for the signout button
         signOutButton.backgroundColor = UIColor.init(red: 231/255, green: 51/255, blue: 55/255, alpha: 1)
         signOutButton.layer.cornerRadius = 25.0
         signOutButton.tintColor = UIColor.white
         
-        //get and display the favorite team and account email
+        // Get and display the favorite team and account email
         emailLabel.text! = (Auth.auth().currentUser?.email)!
         
         let defaults = UserDefaults.standard
         let retrievedFavoriteTeam = defaults.string(forKey: "favoriteTeam")
-        
         favoriteTeamLabel.text? = retrievedFavoriteTeam ?? "None"
         
-        //adds the lines dynamically
-        let lineView = UIView(frame: CGRect(x: 20, y: 190, width: 390, height: 1))
+        let screenWidth = UIScreen.main.bounds.width
+        
+        // Adds the lines dynamically
+        let lineView = UIView(frame: CGRect(x: 20, y: emailLabel.center.y + 26, width: screenWidth - 40, height: 1))
         lineView.layer.borderWidth = 1.0
         lineView.layer.borderColor = UIColor.gray.cgColor
         self.view.addSubview(lineView)
         
-        let lineView2 = UIView(frame: CGRect(x: 20, y: 235, width: 390, height: 1))
+        let lineView2 = UIView(frame: CGRect(x: 20, y: favoriteTeamLabel.center.y + 26, width: screenWidth - 40, height: 1))
         lineView2.layer.borderWidth = 1.0
         lineView2.layer.borderColor = UIColor.gray.cgColor
         self.view.addSubview(lineView2)
         
-        let lineView3 = UIView(frame: CGRect(x: 20, y: 285, width: 390, height: 1))
+        let lineView3 = UIView(frame: CGRect(x: 20, y: themeLabel.center.y + 26, width: screenWidth - 40, height: 1))
         lineView3.layer.borderWidth = 1.0
         lineView3.layer.borderColor = UIColor.gray.cgColor
         self.view.addSubview(lineView3)
         
-        let lineView4 = UIView(frame: CGRect(x: 87, y: 340, width: 325, height: 1))
+        let lineView4 = UIView(frame: CGRect(x: 87, y: systemButton.center.y + 26, width: screenWidth - 107, height: 1))
         lineView4.layer.borderWidth = 1.0
         lineView4.layer.borderColor = UIColor.gray.cgColor
         self.view.addSubview(lineView4)
         
-        let lineView5 = UIView(frame: CGRect(x: 87, y: 390, width: 325, height: 1))
+        let lineView5 = UIView(frame: CGRect(x: 87, y: lightButton.center.y + 26, width: screenWidth - 107, height: 1))
         lineView5.layer.borderWidth = 1.0
         lineView5.layer.borderColor = UIColor.gray.cgColor
         self.view.addSubview(lineView5)
         
-        let lineView6 = UIView(frame: CGRect(x: 87, y: 440, width: 325, height: 1))
+        let lineView6 = UIView(frame: CGRect(x: 87, y: darkButton.center.y + 26, width: screenWidth - 107, height: 1))
         lineView6.layer.borderWidth = 1.0
         lineView6.layer.borderColor = UIColor.gray.cgColor
         self.view.addSubview(lineView6)
